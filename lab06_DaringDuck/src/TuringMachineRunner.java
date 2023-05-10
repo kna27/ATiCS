6af3c833e77cf9afb43efaa432ff7c6838d835d3
matspe24 <matspe24@bergen.org>
2023-05-09 22:12:46 -0400

    fixed input tape lmfao

--- lab06_DaringDuck/src/TuringMachineRunner.java
+++ lab06_DaringDuck/src/TuringMachineRunner.java
@@ -6,7 +6,7 @@ public class TuringMachineRunner {
     Transition startToEndTransition = new Transition('1', Transition.Direction.RIGHT, endState);
     startState.addTransition(startToEndTransition);
     State[] states = { startState, endState };
-    Tape tape = new Tape("0000");
+    Tape tape = new Tape("1");
     StateMachine machine = new StateMachine(states, tape);
     System.out.println("running machine");
     System.out.println(machine.states[0].getTransitions()[0].getSymbol());
